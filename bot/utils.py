b5f9df94550eca6fee65941a03f1994732663567
SAURABH SINGH <saurabhsinghvicky23@gmail.com>
2019-06-02 00:53:46 +0530

    updated utils.py
    added movies utility function to utils

--- bot/utils.py
+++ bot/utils.py
@@ -14,4 +14,32 @@ def get_news(cntry):
 
 
 
+def getmovies(msg):
+    i,t,type,y=msg.split(',')
+    if i=='idk':
+        i=""
+        if t== "idk":
+            datam=dict()
+            datam['valid']="0"
+            datam['reason']="we need atleast name or id of movie"
+            return data
+    elif t== 'idk':
+        if i == "idk":
+            datam=dict()
+            datam['valid']=0
+            datam['reason']="we need atleast name or id of movie"
+            return datam
+        else:
+            i=""
+    elif type == "idk":
+        type=""
+    elif y == 'idk':
+        y=""
+    url="http://www.omdbapi.com/?"
+    header={"apikey":"omdbapi key","i":i,"t":t,"y":y}
+    req=requests.get(url,header)
+    datam=req.json()
+    return datam
+getmovies("idk,avengers endgame,idk,idk")
+get_news("in")
 
